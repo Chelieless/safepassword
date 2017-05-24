a4e92058e909a264f9ceefc2020d997529b3b5bf
Razvan <Razvan@skopeee@gmail.com>
2017-05-24 14:04:51 +0300

    base

---
+++ src/me/Razvan/generator.java
@@ -0,0 +1,5 @@
+package me.Razvan;
+
+public class generator {
+
+}
---
+++ src/me/Razvan/interfata.java
@@ -0,0 +1,24 @@
+package me.Razvan;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class interfata extends Application{
+	
+	public static void main(String[] args) {
+		
+		launch(args);
+		
+	}
+	
+	static Stage window;
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		window = stage;
+		
+		
+		
+	}
+
+}
